a707fa8c0d1eadbd133f265ef069b69c21fe496a
Gavriel Cohen <gavrielcohen0314@gmail.com>
2024-08-17 19:21:00 +0300

    Update __init__.py

--- masking/__init__.py
+++ masking/__init__.py
@@ -3,7 +3,7 @@ import torch
 import math
 
 # You can also import specific functions or classes from submodules
-from .max_loss_data_collator import max_loss_data_colator
-from .min_loss_data_collator import min_loss_data_colator
-from .pseudo_max_perplexity_data_collator import pseudo_max_perplexity_data_collator
-from .whole_word_random_masking_collator import whole_word_random_masking_collator
+from .max_loss_masking import max_loss_data_colator
+from .min_loss_masking import min_loss_data_colator
+from .pseudo_max_perplexity_masking import pseudo_max_perplexity_data_collator
+from .whole_word_random_masking import whole_word_random_data_collator
